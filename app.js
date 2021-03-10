524c0a838d2c7423d07d2f75cc65929bb8c3b2ba
VioletZhouECE <x375zhou@uwaterloo.ca>
2021-03-10 17:31:12 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// JSON parser:
+app.use(express.json());
+
+// test endpoint
+app.get('/test', (req, res) => {
+  const test_msg = {"message" : "hello world"}
+  res.status(200).json(test_msg);
+});
+
+// catch-all endpoint handler
+app.use((req, res) => {
+  return res.status(404).send('wrong endpoint');
+});
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express caught an unkown error!',
+    status: 500,
+    message: { err: 'An error occurred!' },
+  };
+  const errorObj = {...defaultErr, err};
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,13 @@
+const app = require('./app.js');
+const http = require('http');
+
+//set port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+//create http server
+const server = http.createServer(app);
+
+//listen
+server.listen(port);
+console.log("listening on port " + port);
