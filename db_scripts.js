a2efa52665212a9e7a9bcdc794374f64f43b1c34
VioletZhouECE <x375zhou@uwaterloo.ca>
2021-03-16 10:01:55 -0400

    run db scripts when the server starts

--- db_scripts.js
+++ db_scripts.js
@@ -1,31 +1,40 @@
 const Sequelize = require('sequelize');
 const path = require("path");
-const request = require('request');
-const models = require('./models');
 const Umzug = require("umzug");
 require('dotenv').config();
 
-// init db connection
-const sequelize = new Sequelize(process.env.DATABADE, process.env.USERNAME, process.env.PASSWORD, {
-  host: process.env.HOST,
-  dialect: 'postgres'
-});
+const setup = async() => {
+  const sequelize = await connect;
+  console.log(sequelize);
+  await runMigrations(sequelize);
+  await runSeeders(sequelize);
+}
 
-//test the connection
-const connectTest = () => {
-  console.log("Testing connection...");
+//connect to db and return a sequelize instance
+//Note: use Promise here to integrate with sequelize promise-based api
+const connect = new Promise((resolve, reject) => {
+  console.log("Connecting to postgres...");
+
+  // init db connection
+  const sequelize = new Sequelize(process.env.DATABADE, process.env.USERNAME, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: 'postgres'
+  });
+
+  //test connection
   sequelize
     .authenticate()
     .then(() => {
-      console.log('Connection has been established successfully.');
+      console.log('Connection has been established successfully!');
+      resolve(sequelize);
     })
     .catch(err => {
-      console.error('Unable to connect to the database:', err);
+      reject('Unable to connect to the database:', err);
     });
-}
+})
 
 //run all migrations
-const runMigrations = async () => {
+const runMigrations = async (sequelize) => {
   console.log("Running migrations...");
 
   const umzug = new Umzug({
@@ -50,7 +59,7 @@ const runMigrations = async () => {
 }
 
 // insert users to db 
-const runSeeders = async () => {
+const runSeeders = async (sequelize) => {
   console.log("Running seeders...");
 
   const umzug = new Umzug({
@@ -74,5 +83,4 @@ const runSeeders = async () => {
   console.log("All seeders have been executed!");
 }
 
-//runMigrations();
-//runSeeders();
+module.exports = setup
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const app = require('./app.js');
 const http = require('http');
+const setup = require("./db_scripts");
 
-//set port
-const port = process.env.PORT || '3000';
-app.set('port', port);
+//connect to and setup db
+setup().then(() => {
+    
+    //set port
+    const port = process.env.PORT || '3000';
+    app.set('port', port);
 
-//create http server
-const server = http.createServer(app);
+    //create http server
+    const server = http.createServer(app);
 
-//listen
-server.listen(port);
-console.log("listening on port " + port);
+    //listen
+    server.listen(port);
+    console.log("listening on port " + port);
+}
+);
