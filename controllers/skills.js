a8c4467c4b7000100267264321d2f3edecb14f0a
VioletZhouECE <x375zhou@uwaterloo.ca>
2021-03-16 00:30:29 -0400

    working on the getSkills endpoint

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,3 +1,20 @@
-exports.getSkills = (req, res, next)=>{
+const models = require("../models");
+const { QueryTypes } = require('sequelize');
+
+exports.getSkills = async (req, res, next)=>{
+    //hard-coded test data
+    const minFrequency = 5;
+    const maxFrequency = 10;
+
+    //It is easier to do this in the SQL way: join, aggregate, filter and sort
+    const query = `SELECT COUNT(*) FROM skills
+                   INNER JOIN usersSkills 
+                   ON skills.id = usersSkills.skillId
+                   GROUP BY skills.id
+                   HAVING COUNT(*) BETWEEN ${minFrequency} AND ${maxFrequency}
+                   ORDER BY COUNT(*)`;
+    const result = await models.sequelize.query(query, { type: QueryTypes.SELECT });
+    console.log(result);
+
     res.send("get skills");
 }
